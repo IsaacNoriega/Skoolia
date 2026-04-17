4ae677947393fbcf669afec642e2c2f7c05e5e02
unknown <isaac.noriega@iteso.mx>
2026-04-17 15:27:52 -0600

    chore(db): agrega migración para nueva columna

---
+++ apps/api/drizzle/migrations/0020_flimsy_cyclops.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "courses" ADD COLUMN "address" text;--> statement-breakpoint
+ALTER TABLE "courses" ADD COLUMN "city" text;--> statement-breakpoint
+ALTER TABLE "courses" ADD COLUMN "state" text;--> statement-breakpoint
+ALTER TABLE "courses" ADD COLUMN "latitude" double precision;--> statement-breakpoint
+ALTER TABLE "courses" ADD COLUMN "longitude" double precision;
