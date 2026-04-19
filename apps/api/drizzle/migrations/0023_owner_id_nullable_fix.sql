095ba933566e3b06545bdd713e9e82e424b677c8
unknown <isaac.noriega@iteso.mx>
2026-04-18 21:57:37 -0600

    chore: elimina archivos y migraciones obsoletas

--- apps/api/drizzle/migrations/0023_owner_id_nullable_fix.sql
+++
@@ -1,4 +0,0 @@
--- Corrige la columna owner_id para que sea nullable
-ALTER TABLE "courses" DROP COLUMN IF EXISTS "owner_id";
-ALTER TABLE "courses" ADD COLUMN "owner_id" uuid;
--- Ahora puedes poblar los datos y luego hacerla NOT NULL si lo deseas
